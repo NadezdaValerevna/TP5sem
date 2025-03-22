1912639031ef55f5c4372a7c1f41294d2b072e10
NadezdaValerevna <145100132+NadezdaValerevna@users.noreply.github.com>
2025-03-22 16:05:02 +0500

    Add files via upload

---
+++ TextEditor/TextEditor/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace TextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.историяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.восстановитьВерсиюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.SteelBlue;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.закрытьToolStripMenuItem,
+            this.историяToolStripMenuItem,
+            this.восстановитьВерсиюToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            this.создатьToolStripMenuItem.Click += new System.EventHandler(this.создатьToolStripMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // историяToolStripMenuItem
+            // 
+            this.историяToolStripMenuItem.Name = "историяToolStripMenuItem";
+            this.историяToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.историяToolStripMenuItem.Text = "История";
+            this.историяToolStripMenuItem.Click += new System.EventHandler(this.историяToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 422);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 393);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(786, 387);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // восстановитьВерсиюToolStripMenuItem
+            // 
+            this.восстановитьВерсиюToolStripMenuItem.Name = "восстановитьВерсиюToolStripMenuItem";
+            this.восстановитьВерсиюToolStripMenuItem.Size = new System.Drawing.Size(244, 26);
+            this.восстановитьВерсиюToolStripMenuItem.Text = "Восстановить версию";
+            this.восстановитьВерсиюToolStripMenuItem.Click += new System.EventHandler(this.восстановитьВерсиюToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TextEditor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem историяToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem восстановитьВерсиюToolStripMenuItem;
+    }
+}
+
---
+++ TextEditor/TextEditor/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace TextEditor
+{
+    public partial class Form1 : Form
+
+    {
+        // Поле для хранения истории файлов
+        private List<string> recentFiles = new List<string>();
+        private Timer autoSaveTimer = new Timer();
+        private string historyFolder = Path.Combine(Application.StartupPath, "History");
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAutoSave(); // Включаем автосохранение
+        }
+
+        private void InitializeAutoSave()
+        {
+            if (string.IsNullOrWhiteSpace(historyFolder) || !Directory.Exists(historyFolder))
+            {
+                Directory.CreateDirectory(historyFolder);
+            }
+
+            if (!autoSaveTimer.Enabled) // Предотвращаем повторный запуск
+            {
+                autoSaveTimer.Interval = 30000; // 30 секунд
+                autoSaveTimer.Tick += AutoSave_Tick;
+                autoSaveTimer.Enabled = true;
+                autoSaveTimer.Start();
+            }
+        }
+
+        private void AutoSave_Tick(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab == null) return;
+
+            RichTextBox richTextBox = tabControl1.SelectedTab.Controls.OfType<RichTextBox>().FirstOrDefault();
+            if (richTextBox == null || string.IsNullOrWhiteSpace(richTextBox.Text)) return;
+
+            // Очищаем имя файла от недопустимых символов
+            string fileName = tabControl1.SelectedTab.Text;
+            if (string.IsNullOrWhiteSpace(fileName)) fileName = "Untitled";
+            fileName = string.Concat(fileName.Split(Path.GetInvalidFileNameChars()));
+
+            // Создаём имя файла с датой
+            string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            string savePath = Path.Combine(historyFolder, $"{fileName}_{timestamp}.txt");
+
+            try
+            {
+                File.WriteAllText(savePath, richTextBox.Text);
+
+                // Ограничиваем число файлов в папке (оставляем последние 10)
+                string[] savedFiles = Directory.GetFiles(historyFolder, $"{fileName}_*.txt")
+                                               .OrderByDescending(f => f)
+                                               .Skip(10) // Удаляем всё, кроме 10 последних
+                                               .ToArray();
+
+                foreach (string oldFile in savedFiles)
+                {
+                    File.Delete(oldFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка автосохранения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateRecentFiles(string filePath)
+        {
+            // Удаляем файл из списка, если он уже есть (чтобы не дублировался)
+            recentFiles.Remove(filePath);
+
+            // Добавляем в начало списка
+            recentFiles.Insert(0, filePath);
+
+            // Ограничиваем список 5 элементами
+            if (recentFiles.Count > 5)
+            {
+                recentFiles.RemoveAt(5);
+            }
+        }
+
+        private void создатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Создаем новую вкладку с названием
+            string title = "TabPage " + (tabControl1.TabCount + 1).ToString();
+            TabPage newTabPage = new TabPage(title);
+
+            // Создаем новый RichTextBox
+            RichTextBox newRichTextBox = new RichTextBox
+            {
+                Name = "richTextBox" + tabControl1.TabCount, // Даем уникальное имя
+                Dock = DockStyle.Fill // Заполняет всю вкладку
+            };
+
+            // Добавляем RichTextBox на вкладку
+            newTabPage.Controls.Add(newRichTextBox);
+
+            // Добавляем новую вкладку в TabControl
+            tabControl1.TabPages.Add(newTabPage);
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.TabPages.Count > 0)
+            {
+                // Проверяем, есть ли RichTextBox на текущей вкладке
+                RichTextBox richTextBox = tabControl1.SelectedTab.Controls.OfType<RichTextBox>().FirstOrDefault();
+
+                if (richTextBox != null && !string.IsNullOrEmpty(richTextBox.Text))
+                {
+                    // Спрашиваем пользователя, хочет ли он сохранить файл перед закрытием
+                    DialogResult result = MessageBox.Show(
+                        "Сохранить изменения перед закрытием?",
+                        "Сохранение",
+                        MessageBoxButtons.YesNoCancel,
+                        MessageBoxIcon.Warning);
+
+                    if (result == DialogResult.Cancel)
+                    {
+                        return; // Отмена закрытия
+                    }
+                    else if (result == DialogResult.Yes)
+                    {
+                        // Вызываем метод сохранения
+                        сохранитьToolStripMenuItem_Click(sender, e);
+                    }
+                }
+
+                // Закрываем текущую вкладку
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab != null)
+            {
+                RichTextBox richTextBox = tabControl1.SelectedTab.Controls.OfType<RichTextBox>().FirstOrDefault();
+
+                if (richTextBox != null)
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog
+                    {
+                        Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*",
+                        Title = "Сохранить как",
+                        DefaultExt = "txt"
+                    };
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, richTextBox.Text);
+                        tabControl1.SelectedTab.Text = Path.GetFileName(saveFileDialog.FileName);
+
+                        // Добавляем файл в историю
+                        UpdateRecentFiles(saveFileDialog.FileName);
+
+                        MessageBox.Show("Файл успешно сохранен!", "Сохранение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("RichTextBox не найден на текущей вкладке!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Нет открытых вкладок!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*",
+                Title = "Открыть файл"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileContent = File.ReadAllText(openFileDialog.FileName);
+                string fileName = Path.GetFileName(openFileDialog.FileName);
+
+                TabPage newTabPage = new TabPage(fileName);
+                RichTextBox richTextBox = new RichTextBox
+                {
+                    Dock = DockStyle.Fill,
+                    Text = fileContent
+                };
+
+                newTabPage.Controls.Add(richTextBox);
+                tabControl1.TabPages.Add(newTabPage);
+                tabControl1.SelectedTab = newTabPage;
+
+                // Добавляем файл в историю
+                UpdateRecentFiles(openFileDialog.FileName);
+            }
+        }
+
+        private void историяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (recentFiles.Count > 0)
+            {
+                string history = "Последние файлы:\n" + string.Join("\n", recentFiles);
+                MessageBox.Show(history, "История", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("История пуста!", "История", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void восстановитьВерсиюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Проверяем, задан ли historyFolder и существует ли папка, если нет – создаём
+            if (string.IsNullOrWhiteSpace(historyFolder))
+            {
+                MessageBox.Show("Ошибка: Папка истории не задана!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!Directory.Exists(historyFolder))
+            {
+                Directory.CreateDirectory(historyFolder);
+            }
+
+            if (tabControl1.SelectedTab == null)
+            {
+                MessageBox.Show("Ошибка: Не выбрана вкладка!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string fileName = tabControl1.SelectedTab.Text;
+            if (string.IsNullOrWhiteSpace(fileName)) fileName = "Untitled";
+
+            string[] vers = Directory.GetFiles(historyFolder, $"{fileName}_*.txt");
+
+            if (vers.Length == 0)
+            {
+                MessageBox.Show("Нет сохранённых версий!", "История", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Сортировка по номеру версии (если после "_" идёт число)
+            string selectedFile = vers
+                .OrderByDescending(f => int.TryParse(Path.GetFileNameWithoutExtension(f)
+                                                    .Split('_').Last(), out int num) ? num : 0)
+                .First();
+
+            try
+            {
+                string content = File.ReadAllText(selectedFile);
+
+                // Ищем RichTextBox во вкладке
+                RichTextBox richTextBox = tabControl1.SelectedTab.Controls.OfType<RichTextBox>().FirstOrDefault();
+                if (richTextBox == null)
+                {
+                    MessageBox.Show("Ошибка: Вкладка не содержит текстового поля!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                richTextBox.Text = content;
+                MessageBox.Show($"Восстановлено из: {Path.GetFileName(selectedFile)}", "Восстановление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при чтении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
